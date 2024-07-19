6e8c6b07ccfddd5bbac483cd154856c98da3b5df
Nihar Reddy <niharreddy3@gmail.com>
2024-07-19 00:21:49 -0500

    03 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function (input) {
+  let result = "";
+  for (let i = input.length - 1; i >= 0; i--) {
+    result = result.concat(input.charAt(i));
+  }
 
+  return result;
 };
 
 // Do not edit below this line
