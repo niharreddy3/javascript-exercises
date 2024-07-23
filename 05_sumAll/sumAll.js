b8e6964ee6163dc5861e168ea44b599799518f08
Nihar Reddy <niharreddy3@gmail.com>
2024-07-22 22:40:08 -0500

    ex.5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    typeof num1 !== "number" ||
+    typeof num2 !== "number" ||
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2) ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  }
 
+  if (num1 > num2) {
+    let temp = num2;
+    num2 = num1;
+    num1 = temp;
+  }
+
+  let i = num1;
+  let result = 0;
+
+  for (i; i <= num2; i++) {
+    result += i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
