b2a4d547bd23f51e9f1509876c96cd3d08ca2b78
Nihar Reddy <niharreddy3@gmail.com>
2024-07-22 23:00:28 -0500

    06 complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function (input) {
+  if (input % 100 === 0 && input % 400 === 0) {
+    return true;
+  } else if (input % 100 === 0 && input % 400 !== 0) {
+    return false;
+  } else if (input % 4 === 0) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
