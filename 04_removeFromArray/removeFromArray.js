06d9970ac22684d3ad7c0b9e06e37b900e20baaf
Nihar Reddy <niharreddy3@gmail.com>
2024-07-22 23:26:37 -0500

    ex 04 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (...theArgs) {
+  const arr = theArgs[0];
+
+  for (let i = 1; i < theArgs.length; i++) {
+    while (arr.includes(theArgs[i])) {
+      let indexToDelete = arr.indexOf(theArgs[i]);
+      if (indexToDelete !== -1) {
+        arr.splice(indexToDelete, 1);
+      }
+    }
+  }
+
+  return arr;
 };
 
 // Do not edit below this line
