c6371898efe4f71bf826d589160cb1ced199207d
Nihar Reddy <niharreddy3@gmail.com>
2024-08-03 18:09:58 -0500

    ex10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (Number(num) === 0) {
+    return 0;
+  }
 
+  if (Number(num) === 1) {
+    return 1;
+  }
+
+  if (Number(num) < 0) {
+    return "OOPS";
+  }
+
+  let arr = [1, 1];
+
+  while (arr.length < Number(num)) {
+    arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+  }
+
+  return arr[arr.length - 1];
 };
 
 // Do not edit below this line
