46a932d5ceeb5ae0d4f20057239526c1f204900a
Nihar Reddy <niharreddy3@gmail.com>
2024-08-03 17:34:25 -0500

    ex 12 inital complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let sortedByAge = people.sort((a, b) => {
+    if (a.yearOfDeath === undefined) {
+      a.yearOfDeath = new Date().getFullYear();
+    }
 
+    if (b.yearOfDeath === undefined) {
+      b.yearOfDeath = new Date().getFullYear();
+    }
+    return b.yearOfDeath - b.yearOfBirth - (a.yearOfDeath - a.yearOfBirth);
+  });
+
+  return sortedByAge[0];
 };
 
 // Do not edit below this line
