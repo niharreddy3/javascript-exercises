6fee2b2b1fa84322e35fad44d41a1f3bcfad849c
Nihar Reddy <niharreddy3@gmail.com>
2024-08-03 15:25:27 -0500

    Init 08 complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  return arr.reduce((acc, itr) => acc + itr);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((acc, itr) => (acc *= itr));
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +37,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
