e28b82c2b5848054cedaffad9de288bb3c75981a
Nihar Reddy <niharreddy3@gmail.com>
2024-08-03 21:50:20 -0500

    init sol for 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,35 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let leftPtr = 0;
+  let rightPtr = str.length - 1;
 
+  while (leftPtr < rightPtr) {
+    while (
+      str[leftPtr] === "!" ||
+      str[leftPtr] === "," ||
+      str[leftPtr] === " " ||
+      str[leftPtr] === "."
+    ) {
+      leftPtr++;
+    }
+
+    while (
+      str[rightPtr] === "!" ||
+      str[rightPtr] === "," ||
+      str[rightPtr] === " " ||
+      str[rightPtr] === "."
+    ) {
+      rightPtr--;
+    }
+
+    if (str[leftPtr].toLowerCase() !== str[rightPtr].toLowerCase()) {
+      return false;
+    } else {
+      leftPtr++;
+      rightPtr--;
+    }
+  }
+
+  return true;
 };
 
 // Do not edit below this line
